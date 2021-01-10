84c525eccf26a570f791c8e19f250eb9c688765e
90stareq <mhtareqarz@gmail.com>
2021-01-10 16:19:11 +0600

    script fun added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorPickerImg from "../src/assets/colorpicker.png";
 import ColorPicker from "./components/ColorPicker";
 import "./App.css";
@@ -7,6 +7,16 @@ export default function App() {
   const [inputColor, setInputColor] = useState("");
   const [colorName, setcolorName] = useState("");
   const [isEnable, setIsEnable] = useState(false);
+  useEffect(() => {
+    includeLi();
+  }, []);
+
+  //Script
+  const includeLi = () => {
+    let theul = document.querySelector(".variable-items-wrapper");
+    let theli = document.querySelector(".newli");
+    theul.appendChild(theli);
+  };
 
   //Store InputBackground Color
   const inputBGColor = (color, name) => {
@@ -18,7 +28,50 @@ export default function App() {
     <div className="main">
       <input type="text" value={inputColor} name="hood-rgb-color" />
       <input type="text" value={colorName} name="hood-rgb-name" />
-
+      <ul className="variable-items-wrapper">
+        <li
+          onClick={() => setIsEnable(!isEnable)}
+          data-wvstooltip="Antique White"
+          className="variable-item color-variable-item color-variable-item-antique-white nn"
+          title="Antique White"
+          data-value="antique-white"
+          role="button"
+          tabindex="0"
+        >
+          <span
+            className="variable-item-span variable-item-span-color"
+            style={{ background: "red" }}
+          ></span>
+        </li>
+        <li
+          onClick={() => setIsEnable(!isEnable)}
+          data-wvstooltip="Antique White"
+          className="variable-item color-variable-item color-variable-item-antique-white nn"
+          title="Antique White"
+          data-value="antique-white"
+          role="button"
+          tabindex="0"
+        >
+          <span
+            className="variable-item-span variable-item-span-color"
+            style={{ background: "red" }}
+          ></span>
+        </li>
+        <li
+          onClick={() => setIsEnable(!isEnable)}
+          data-wvstooltip="Antique White"
+          className="variable-item color-variable-item color-variable-item-antique-white nn"
+          title="Antique White"
+          data-value="antique-white"
+          role="button"
+          tabindex="0"
+        >
+          <span
+            className="variable-item-span variable-item-span-color"
+            style={{ background: "red" }}
+          ></span>
+        </li>
+      </ul>
       <li
         onClick={() => setIsEnable(!isEnable)}
         data-wvstooltip="Antique White"
