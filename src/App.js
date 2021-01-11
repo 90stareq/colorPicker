967a0927e6d19b57576feae99015ccaaf7cd22be
90stareq <mhtareqarz@gmail.com>
2021-01-11 10:11:15 +0600

    removed img button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,79 @@
 import React, { useEffect, useState } from "react";
+import colorslist from "../src/components/ColorsList";
 import ColorPickerImg from "../src/assets/colorpicker.png";
 import ColorPicker from "./components/ColorPicker";
 import "./App.css";
 
 export default function App() {
-  const [inputColor, setInputColor] = useState("");
+  // const [inputColor, setInputColor] = useState("");
+  // const [colorName, setcolorName] = useState("");
+  // const [isEnable, setIsEnable] = useState(false);
+  // useEffect(() => {
+  //   includeLi();
+  // }, []);
+
+  // //Script
+  // const includeLi = () => {
+  //   let theul = document.querySelector(".variable-items-wrapper");
+  //   let theli = document.querySelector(".newli");
+  //   theul.appendChild(theli);
+  // };
+
+  // //Store InputBackground Color
+  // const inputBGColor = (color, name) => {
+  //   setInputColor(color);
+  //   setcolorName(name);
+  // };
+
+  const [colors, setColors] = useState([]);
+  const [color, setColor] = useState("");
+  const [search, setSearch] = useState("");
+  const [filteredColors, setFilteredColors] = useState([]);
   const [colorName, setcolorName] = useState("");
-  const [isEnable, setIsEnable] = useState(false);
+
   useEffect(() => {
-    includeLi();
+    setColors(colorslist);
   }, []);
 
-  //Script
-  const includeLi = () => {
-    let theul = document.querySelector(".variable-items-wrapper");
-    let theli = document.querySelector(".newli");
-    theul.appendChild(theli);
-  };
-
-  //Store InputBackground Color
-  const inputBGColor = (color, name) => {
-    setInputColor(color);
-    setcolorName(name);
-  };
+  useEffect(() => {
+    setFilteredColors(
+      colors.filter((country) =>
+        country.colorName.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, colors]);
 
   return (
-    <div className="main">
-      <input type="text" value={inputColor} name="hood-rgb-color" />
+    <div className="main hoodsly-colorpicker-main">
+      <input type="text" value={color} name="hood-rgb-color" />
+      <input type="text" value={colorName} name="hood-rgb-name" />
+      <div className="colors_box">
+        <div className="colors">
+          <input
+            type="text"
+            placeholder="Search Bar"
+            onChange={(e) => setSearch(e.target.value)}
+            className="search_input"
+          />
+          <span style={{ backgroundColor: color }} id="selectedcolor"></span>
+          <div className="single-color-list">
+            {filteredColors.map((c, i) => {
+              return (
+                <div
+                  key={i}
+                  onClick={() => {
+                    setColor(c.colorCode);
+                    setcolorName(c.colorName);
+                  }}
+                  style={{ background: c.colorCode }}
+                  className="single_color"
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {/* <input type="text" value={inputColor} name="hood-rgb-color" />
       <input type="text" value={colorName} name="hood-rgb-name" />
       <li
         onClick={() => setIsEnable(!isEnable)}
@@ -52,7 +99,7 @@ export default function App() {
 
       {isEnable ? (
         <ColorPicker inputColor={inputColor} inputBGColor={inputBGColor} />
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
